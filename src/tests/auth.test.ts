014a9409874b71d5ba959defb21afba7f0621775
alaa-dere <alaadere35@gmail.com>
2026-03-13 15:14:10 +0200

    cd workflow

--- src/tests/auth.test.ts
+++ src/tests/auth.test.ts
@@ -1,5 +1,5 @@
 import { describe, test, expect } from "vitest";
-import { getAPIKey } from "../api/auth";
+import { getAPIKey } from "../api/auth.js";
 describe("getAPIKey", () => {
   test("when header is valid", () => {
     const headers = {
