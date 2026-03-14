2717e3216c4520ce0976a6e0189392c2507cf570
alaa-dere <alaadere35@gmail.com>
2026-03-14 21:50:51 +0200

    add database migrations to cd workflow

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,6 +5,7 @@ import { config } from "./src/config";
 export default defineConfig({
   out: "./src/db/migrations",
   schema: "./src/db/schema.ts",
+  connectionString: process.env.DATABASE_URL,
   dialect: "turso",
   dbCredentials: {
     url: config.db.url || "",
